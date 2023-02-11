17bc3a3e6d42391f14bb05977956faaa966b68ec
Pathey70 <34804740+Pathey70@users.noreply.github.com>
2023-02-11 13:09:06 -0500

    Time: 61 ms (47.00%), Space: 43.2 MB (62.57%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        TreeMap<Integer,Integer> hm=new TreeMap<>();
+        for(Integer i:hand){
+            hm.put(i,hm.getOrDefault(i,0)+1);
+        }
+        while(!hm.isEmpty()){
+            int x1=hm.firstKey();
+            for(int i=x1;i<x1+groupSize;i++){
+                int t1=hm.getOrDefault(i,-1);
+                if(t1==-1)
+                    return false;
+                if(t1==1)
+                    hm.remove(i);
+                else
+                    hm.put(i,t1-1);
+            }
+        }
+        return true;
+    }
+}
