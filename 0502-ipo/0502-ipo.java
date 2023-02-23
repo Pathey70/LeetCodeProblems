77c755656118fb3965550f389302979ae63c70f9
Pathey70 <34804740+Pathey70@users.noreply.github.com>
2023-02-22 20:42:30 -0500

    Time: 126 ms (33.02%), Space: 103.2 MB (9.87%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -23,53 +23,6 @@ class Solution {
             }
         }
         return (int)c;
-        /*Queue<Cap> queue=new PriorityQueue<>(profits.length,new ListComparator());
-        for(int i=0;i<profits.length;i++){
-            
-                Cap cap=new Cap();
-                cap.x=profits[i];
-                cap.y=capital[i];
-                queue.add(cap);
-            
-        }
-        int i=0;
-        while(i<k&&queue.size()>0){
-            Stack<Cap> stack=new Stack<>();
-            boolean found=false;
-            while(true&&queue.size()>0){
-               Cap cap=queue.poll();
-            //   System.out.println(cap.x+" "+cap.y);
-                if(cap.y<=w){
-                    
-                    found=true;
-                    w+=cap.x;
-                    break;
-                }
-                else{
-                    stack.add(cap);
-                }
-            }
-            if(found)
-            {
-                while(!stack.isEmpty()){
-                    queue.add(stack.pop());
-                }
-                i++;
-            }
-            else
-            {
-                return w;
-            }
-        }
-        return w;*/
-    }
-}
-class Cap{
-    int x;
-    int y;
-}
-class ListComparator implements Comparator<Cap>{
-    public int compare(Cap l1,Cap l2){
-        return Integer.compare(l2.x,l1.x);
+      
     }
 }
