51242d5ea9802b2403c36ed7c600ef114b47c18d
Pathey70 <34804740+Pathey70@users.noreply.github.com>
2023-03-05 09:23:25 -0500

    Time: 93 ms (43.07%), Space: 50.2 MB (100.00%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        int n=arr.length;
+        int dp[]=new int[n];
+        for(int i=0;i<n-1;i++)
+            dp[i]=Integer.MAX_VALUE-1;
+        boolean flag=true;
+        HashMap<Integer,Integer> right=new HashMap<>();
+        right.put(arr[n-1],0);
+        while(flag){
+            flag=false;
+            for(int i=n-2;i>=0;i--){
+                int min=dp[i+1]+1;
+                min=Math.min(min,i>0 ? dp[i-1]+1 : Integer.MAX_VALUE);
+                min=Math.min(min,right.getOrDefault(arr[i],Integer.MAX_VALUE-1)+1);
+                if(min<dp[i])
+                {   
+                    dp[i]=min;
+                    flag=true;
+                    right.put(arr[i],Math.min(dp[i],right.getOrDefault(arr[i],Integer.MAX_VALUE)));
+                }
+            }
+        }
+        return dp[0];
+    }
+}
