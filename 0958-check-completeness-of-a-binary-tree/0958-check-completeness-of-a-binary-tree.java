fca3b27714482f011dafa99f12d362fa52a51f04
Pathey70 <34804740+Pathey70@users.noreply.github.com>
2023-03-15 01:02:10 -0400

    Time: 1 ms (83.31%), Space: 42.3 MB (15.55%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -17,63 +17,6 @@ class Solution {
     int heightFinal=0;
     boolean leaf=false;
     public boolean isCompleteTree(TreeNode root) {
-       /* if(root==null)
-        {
-            return true;
-        }
-        Queue<TreeNode> queue=new LinkedList<TreeNode>();
-        queue.add(root);
-        int count=0;
-        boolean full=true;
-        int fullCount=-1;
-        int fullLevel=-1;
-        while(queue.isEmpty()==false)
-        {
-            TreeNode element=queue.remove();
-            count++;
-            TreeNode left=element.left;
-            TreeNode right=element.right;
-            System.out.println(count);
-            if(element.left==null&&element.right!=null)
-                return false;
-            else if(element.left!=null&&element.right==null)
-            {
-                if(full)
-                {
-                    full=false;
-                    fullCount=count;
-                    fullLevel=(int )(Math.log(count)/Math.log(2));
-                    //count++;
-                    queue.add(element.left);
-                    System.out.println(fullLevel+" fullLevel");
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else if(left==null&&right==null)
-            {
-                if(full)
-                {
-                    full=false;
-                    fullCount=count;
-                    fullLevel=(int )(Math.log(count)/Math.log(2));
-                    System.out.println(fullLevel);
-                }
-                
-            }
-            else
-            {
-                if(!full)
-                {
-                    return false;
-                }
-                queue.add(element.left);
-                queue.add(element.right);
-            }
-            
-        }*/
         if(root==null)
         {
             return true;
@@ -88,27 +31,6 @@ class Solution {
                     return false;
             }
             else{
-                /*if(node.left==null||node.right==null)
-                {   
-                    if(node.left!=null){
-                        queue.add(node.left);
-                    }
-                    else{
-                        stoppage=true;
-                    }
-                    if(node.right!=null)
-                    {   
-                        if(stoppage)
-                            return false;
-                        stoppage=true;
-                        queue.add(node.right);
-                    }
-                }
-                else{
-                    queue.add(node.left);
-                    queue.add(node.right);
-                }
-                */
                 if(node.left!=null)
                     queue.add(node.left);
                 else 
@@ -126,7 +48,6 @@ class Solution {
             }
         }
         return true;
-        
     }
     
 }
