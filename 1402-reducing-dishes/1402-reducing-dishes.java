db05314d8afb2b8470de4db52c4f9be59f9897f2
Pathey70 <34804740+Pathey70@users.noreply.github.com>
2023-03-29 19:34:01 -0400

    Time: 1 ms (100.00%), Space: 40.6 MB (72.82%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,23 +1,27 @@
 class Solution {
     int ans=0;
     public int maxSatisfaction(int[] satisfaction) {
-        int n=satisfaction.length;
-        int dp[][]=new int[n][n+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=n;j++)
-                dp[i][j]=-1;
-        }
         Arrays.sort(satisfaction);
-        return rec(satisfaction,0,1,dp);
-        
-    }
-    public int rec(int[] satisfaction,int i,int o,int dp[][]){
-        if(i==satisfaction.length)
-            return 0;
-        if(dp[i][o]>=0)
-            return dp[i][o];
-        dp[i][o]=Math.max(o*satisfaction[i]+rec(satisfaction,i+1,o+1,dp),rec(satisfaction,i+1,o,dp));
-        //System.out.println(dp[i][o]+" "+i+" "+o);
-        return dp[i][o];
+        int n=satisfaction.length;
+        int pSum=0,nSum=0;
+        for(int i=0;i<n;i++){
+            if(satisfaction[i]>0)  
+                pSum+=satisfaction[i];
+            else
+                nSum+=satisfaction[i];
+        }
+        int i=0;
+        int ans=0;
+        int count=1;
+        while(i<n&&pSum+nSum<0){
+            nSum-=satisfaction[i];
+            i++;
+        }
+        while(i<n){
+            ans+=satisfaction[i]*count;
+            count++;
+            i++;
+        }
+        return ans;
     }
 }
