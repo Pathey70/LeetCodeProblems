5d298835b9b321210da32d2dc0d1d91efd40bd54
Pathey70 <34804740+Pathey70@users.noreply.github.com>
2024-08-05 14:23:51 -0400

    Time: 88 ms (39.71%), Space: 61.8 MB (6.45%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,16 +1,16 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        TreeMap<Integer,Double> map=new TreeMap<>();
+        TreeMap<Integer,Double> map=new TreeMap<>(Collections.reverseOrder());
         for(int i=0;i<position.length;i++)
         {
             map.put(position[i],(target-position[i]-0.0)/speed[i]);
         }
-        Set<Integer> hs=map.descendingKeySet();
+        
         double current=0;
         int groups=0;
-        for(Integer i:hs)
+        for(Double time:map.values())
         {
-            double time=map.get(i);
+            //double time=map.get(i);
             if(time>current)
             {
                 groups++;
