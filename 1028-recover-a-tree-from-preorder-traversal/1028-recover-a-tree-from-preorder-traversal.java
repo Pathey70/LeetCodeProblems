5a90574a78d6431663502837a0bf19ca16de768b
Pathey70 <34804740+Pathey70@users.noreply.github.com>
2024-10-27 21:32:55 -0400

    Time: 5 ms (62.50%), Space: 44.8 MB (33.02%) - LeetHub

---
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode recoverFromPreorder(String traversal) {
+        
+        
+        return generateTree(traversal,0,traversal.length(),1);
+        
+    }
+    public TreeNode generateTree(String traversal,int start,int end,int d)
+    {
+        if(start>end)
+            return null;
+        TreeNode root=new TreeNode(getNumber(traversal,start,end));
+        int len=0;
+        int ls=-1;
+        int rs=-1;
+        for(int j=start;j<end;j++)
+        {
+            if(traversal.charAt(j)=='-')
+                len++;
+            else
+            {
+                if(len==d)
+                {
+                    if(ls<0)
+                        ls=j;
+                    else if(rs<0)
+                    {
+                        rs=j;
+                        break;
+                    }
+                }
+                len=0;
+            }
+        }
+       // System.out.println(ls+" "+rs);
+        if(ls!=-1&&rs!=-1)
+        {
+            root.left=generateTree(traversal,ls,rs,d+1);
+            root.right=generateTree(traversal,rs,end,d+1);
+        }
+        else if(ls!=-1)
+        {
+            root.left=generateTree(traversal,ls,end,d+1);
+        }
+        else if(rs!=-1)
+        {
+            root.right=generateTree(traversal,rs,end,d+1);
+        }
+        
+        return root;
+    }
+    
+    public int getNumber(String s,int idx,int end)
+    {
+        int i=idx;
+        while(i<end&&s.charAt(i)>='0'&&s.charAt(i)<='9')
+            i++;
+        return Integer.parseInt(s.substring(idx,i));
+    }
+}
